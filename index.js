52ad4a9cb6c65fa7882a37888abf03f26457b7de
James Snell <vulpes3.1415@yahoo.com>
2023-12-26 15:35:11 -0700

    revered word

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  use strings and comparison operator
 */
 
 /*
   Add written explanation of your solution here
+  Use string method .split to seperate letters. This converts string letters into elements of an array. Then array method .reverse to flip order. Array method .join to return array as a string. 
 */
 
 // You can run `node index.js` to view these console logs
